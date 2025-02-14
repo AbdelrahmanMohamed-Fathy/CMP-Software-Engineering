d021e399566e5a37cc8df129fde51482784da389
AbdelrahmanMohamed-Fathy <abdelrahman.fathy04@outlook.com>
2025-02-14 19:15:54 +0200

    assignment done

--- Lab#1/BE/controllers/employee.js
+++ Lab#1/BE/controllers/employee.js
@@ -1,5 +1,7 @@
 const employee = [
   { id: '1', name: 'Mohamed Sayed' },
+  { id: '2', name: 'Abdelrahman Fathy' },
+  { id: '3', name: 'test' }
 ];
 
 exports.getEmployees = async (req, res, next) => {
@@ -8,8 +10,14 @@ exports.getEmployees = async (req, res, next) => {
 
 // TODO
 exports.deleteEmployee = async (req, res, next) => {
+  let employeeToDelete = employee.findIndex(e => e.id == req.params.id)
+  console.log("Deleted:", employee.splice(employeeToDelete, 1))
+  res.status(200).json({ message: "successful" })
 };
 
 // TODO
 exports.createEmployee = async (req, res, next) => {
+  employee.push(req.body)
+  console.log("Added:", req.body)
+  res.status(200).json({ message: "successful" })
 };
--- Lab#1/FE/index.js
+++ Lab#1/FE/index.js
@@ -16,9 +16,12 @@ function fetchEmployees() {
         row.appendChild(nameCell)
 
         const deleteCell = document.createElement('td')
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.classList.add('btn', 'btn-danger', 'btn-sm');
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = 'Delete'
+        deleteButton.classList.add('btn', 'btn-danger', 'btn-sm')
+        // TODO
+        // add event listener to delete button
+        deleteCell.addEventListener("click", () => { deleteEmployee(item.id) })
         deleteCell.appendChild(deleteButton);
 
         row.appendChild(deleteCell)
@@ -31,22 +34,47 @@ function fetchEmployees() {
 
 // TODO
 // add event listener to submit button
+document.getElementById("submitbtn").addEventListener("click", createEmployee)
+
+
 
 // TODO
-// add event listener to delete button
-
-// TODO
-function createEmployee (){
+function createEmployee() {
   // get data from input field
   // send data to BE
   // call fetchEmployees
+  let name = document.getElementById("name").value
+  let id = document.getElementById("id").value
+  console.log(id, name)
+  if (name == "" || id == "")
+    return
+  fetch("http://localhost:3000/api/v1/employee/",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        id: id,
+        name: name
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }
+  ).then(response => response.json())
+    .then(fetchEmployees)
 }
 
 // TODO
-function deleteEmployee (){
+function deleteEmployee(id) {
   // get id
   // send id to BE
   // call fetchEmployees
+  fetch("http://localhost:3000/api/v1/employee/" + id,
+    {
+      method: "DELETE"
+    }
+  ).then(response => { response.json() })
+    .then(fetchEmployees)
+
 }
 
 fetchEmployees()
